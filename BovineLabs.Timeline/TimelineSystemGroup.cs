0768b3eacba2c2cfcddda4897a785b6d0a3c3768
Timothy Raines <tim@ninjatertle.com>
2024-03-17 13:55:21 +1100

    Updated core.

--- BovineLabs.Timeline/TimelineSystemGroup.cs
+++ BovineLabs.Timeline/TimelineSystemGroup.cs
@@ -4,9 +4,11 @@
 
 namespace BovineLabs.Timeline
 {
+    using BovineLabs.Core.Groups;
     using Unity.Entities;
 
     [WorldSystemFilter(WorldSystemFilterFlags.Default | WorldSystemFilterFlags.Editor, WorldSystemFilterFlags.Default | WorldSystemFilterFlags.Editor)]
+    [UpdateInGroup(typeof(BeforeTransformSystemGroup))]
     public partial class TimelineSystemGroup : ComponentSystemGroup
     {
     }
